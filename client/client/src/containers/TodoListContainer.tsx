581be96f52c5db6a56a691032c4fdacd47ce79a6
Roman Alekseev <alexsilalov@gmail.com>
2024-07-05 14:44:42 +0300

    TodoListContainer.tsx

--- client/client/src/containers/TodoListContainer.tsx
+++ client/client/src/containers/TodoListContainer.tsx
@@ -2,10 +2,7 @@ import * as React from 'react';
 import TodoList from 'components/TodoList';
 import { connect } from 'react-redux';
 import { StoreState } from 'store/modules';
-import { 
-  TodoItemData,
-  actionCreators as todosActions
-} from 'store/modules/todos';
+import { TodoItemData, actionCreators as todosActions } from 'store/modules/todos';
 import { bindActionCreators } from 'redux';
 import { List } from 'immutable';
 
@@ -19,20 +16,20 @@ class TodoListContainer extends React.Component<Props> {
   onCreate = () => {
     const { TodosActions, input } = this.props;
     TodosActions.create(input);
-  }
+  };
   onRemove = (id: number) => {
     const { TodosActions } = this.props;
     TodosActions.remove(id);
-  }
+  };
   onToggle = (id: number) => {
     const { TodosActions } = this.props;
     TodosActions.toggle(id);
-  }
+  };
   onChange = (e: React.FormEvent<HTMLInputElement>) => {
     const { value } = e.currentTarget;
     const { TodosActions } = this.props;
     TodosActions.changeInput(value);
-  }
+  };
   render() {
     const { input, todoItems } = this.props;
     const { onCreate, onRemove, onToggle, onChange } = this;
@@ -55,7 +52,7 @@ export default connect(
     input: todos.input,
     todoItems: todos.todoItems,
   }),
-  (dispatch) => ({
+  dispatch => ({
     TodosActions: bindActionCreators(todosActions, dispatch),
-  })
+  }),
 )(TodoListContainer);
